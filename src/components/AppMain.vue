98cdc530079cf8426a509d0741ca53fad0a44fa5
mariocstweb <mariocstmra@gmail.com>
2024-02-02 15:38:09 +0100

    added MainContent component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,11 @@
 <script>
+import MainContent from './MainContent.vue'
 export default {
-  name: "AppMain"
+  name: "AppMain",
+  components: {
+    MainContent
+  }
+
 };
 </script>
 
@@ -8,18 +13,48 @@ export default {
   <main>
     <div class="container">
       <h2>--Content goes here--</h2>
+      <div class="card-container">
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+        <MainContent></MainContent>
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped>
 main {
-  background-color: black;
-  height: calc(100vh - 550px);
+  background-color: #1C1C1C;
   display: flex;
+  justify-content: center;
 }
 
 h2 {
   margin: 0;
+}
+
+
+
+.card-container {
+  max-width: 1300px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 30px;
+}
+
+.series-card {
+  flex-basis: calc((100% / 6) - 30px);
+
 }
 </style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "MainContent"
+};
+</script>
+
+
+
+<template>
+  <div class="series-card">
+    <img
+      src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY"
+      alt="">
+    <p>Ciao</p>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.series-card {
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  ;
+
+  p {
+    color: white;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin-bottom: 50px;
+  }
+}
+</style>
